5f548f40c1991f1c8e672c07bde21d271cb09105
tfornal <tomasz.fornal6@gmail.com>
2023-06-29 22:22:53 +0200

    runner improved

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import time
 from reader import get_discharge_nr_from_csv
+import pandas as pd
+import pathlib
 
 start = time.time()
 
-element = "C"
-date = "20230215"
-discharges = [32]
-time_interval = [0.01, 2]
-dt = 5e-3
+elements = ["O"]
+date = "20230214"
+discharges = [i for i in range(100)]
+time_interval = [0, 600]
+
+
+def get_from_csv(element, date):
+    filepath = pathlib.Path.cwd() / "discharge_numbers" / f"{element}-{date}.csv"
+    df = pd.read_csv(filepath)
+
 
 if __name__ == "__main__":
-    for shot in discharges:
-        get_discharge_nr_from_csv(element, date, shot, time_interval, dt, plotter=True)
+    for element in elements:
+        for shot in discharges:
+            get_discharge_nr_from_csv(element, date, shot, time_interval, plotter=True)
 
 fe = time.time() - start
 format_float = "{:.2f}".format(fe)
