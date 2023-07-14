066c83ba7a4ca83c67ff4490e8c2f58c444f9c98
tfornal <tomasz.fornal6@gmail.com>
2023-07-14 22:12:21 +0200

    cleanup

--- src/intensity.py
+++ src/intensity.py
@@ -260,28 +260,3 @@ def get_discharge_nr_from_csv(element, date, discharge_nr, time_interval, plotte
 
         if plotter:
             plot()
-
-
-import time
-from intensity import get_discharge_nr_from_csv
-import pandas as pd
-import pathlib
-
-start = time.time()
-
-elements = ["C", "O"]  # , "O"]  # , "O"]
-date = "20230215"
-
-
-discharges = [15]  # , 18, 21]
-# discharges = [0]
-time_interval = [0, 415]
-
-
-if __name__ == "__main__":
-    for shot in discharges:
-        for element in elements:
-            get_discharge_nr_from_csv(element, date, shot, time_interval, plotter=True)
-
-
-#### na osiach rowniey dodac cyasz lokalne z eksperymentow - czas rozpoczecia i zakonczenia, a takze ewentualnie dla sprawdzenia utc
