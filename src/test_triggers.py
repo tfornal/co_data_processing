600dd13e1b367bff3de497dccd422beaba0a6ff7
tfornal <tomasz.fornal6@gmail.com>
2023-08-25 17:04:00 +0200

    assertions to be corrected, failure test to be included

--- src/test_triggers.py
+++ src/test_triggers.py
@@ -9,7 +9,6 @@ def get_test_files_paths():
     path = path_manager.program_triggers()
     files = [file.name for file in path.glob("*_triggers.csv")]
     # for file in files:
-    # path = FilePathManager(None, file).program_triggers() / file
     return [FilePathManager(None, file).program_triggers() / file for file in files]
 
 
@@ -19,18 +18,17 @@ def test_if_not_empty(get_test_files_paths):
 
 def test_trigger_columns(get_test_files_paths):
     for path in get_test_files_paths:
-        # path = FilePathManager(None, file).program_triggers() / file
         df = pd.read_csv(path, sep="\t")
         T0 = df["T0"]
         T1 = df["T1"]
         T6 = df["T6"]
+        # TODO - zle warunki ponizej
         assert (T0 >= 0).all()
         assert (T1 >= 0).all()
 
 
 def test_sequent_rows(get_test_files_paths):
     for path in get_test_files_paths:
-        # path = FilePathManager(None, file).program_triggers() / file
         df = pd.read_csv(path, sep="\t")
         array = df.to_numpy()[:, 2:]
         for i, arr_line in enumerate(array):
@@ -39,3 +37,6 @@ def test_sequent_rows(get_test_files_paths):
             x = arr_line - array[i - 1]
             print(x)
             assert x.all() >= 0
+
+
+## napisac funkcje, ktora nie przejdzie testow - fakeowy csv
