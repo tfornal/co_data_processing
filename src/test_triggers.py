a7da6c6d9fd942d453ab87a3e70e655979c01e7e
tfornal <tomasz.fornal6@gmail.com>
2023-08-25 16:42:43 +0200

    first valid tests

--- src/test_triggers.py
+++ src/test_triggers.py
@@ -1,33 +1,33 @@
-import numpy as np
 import pandas as pd
 import pytest
-
 from file_reader import FilePathManager
 
 
-def test_trigger_columns(date):
-    path = FilePathManager(None, date).program_triggers() / f"{date}_triggers.csv"
-    df = pd.read_csv(path, sep="\t")
-    print(df)
-    T0 = df["T0"]
-    T1 = df["T1"]
-    T6 = df["T6"]
-    assert T0.all() >= 0
-    assert T0.all() >= 0
+@pytest.fixture
+def get_test_files():
+    path_manager = FilePathManager()
+    path = path_manager.program_triggers()
+    return [file.name for file in path.glob("*.csv")]
 
 
-def test_sequent_rows(date):
-    path = FilePathManager(None, date).program_triggers() / f"{date}_triggers.csv"
-    df = pd.read_csv(path, sep="\t")
-    array = df.to_numpy()[:, 2:]
-    for i, arr_line in enumerate(array):
-        if i == 0:
-            continue
-        x = arr_line - array[i - 1]
-        assert x.all() > 0
+def test_trigger_columns(get_test_files):
+    for file in get_test_files:
+        path = FilePathManager(None, file).program_triggers() / file
+        df = pd.read_csv(path, sep="\t")
+        T0 = df["T0"]
+        T1 = df["T1"]
+        T6 = df["T6"]
+        assert (T0 >= 0).all()
+        assert (T1 >= 0).all()
 
 
-if __name__ == "__main__":
-    date = "20230209"
-    test_trigger_columns(date)
-    test_sequent_rows(date)
+def test_sequent_rows(get_test_files):
+    for file in get_test_files:
+        path = FilePathManager(None, file).program_triggers() / file
+        df = pd.read_csv(path, sep="\t")
+        array = df.to_numpy()[:, 2:]
+        for i, arr_line in enumerate(array):
+            if i == 0:
+                continue
+            x = arr_line - array[i - 1]
+            assert x.all() >= 0
