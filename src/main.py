bbcd65866b0dd1f6094abab673a8c217ef326280
tfornal <tomasz.fornal6@gmail.com>
2023-08-27 21:59:21 +0200

    cleanup and improved plotting of compared data

--- src/main.py
+++ src/main.py
@@ -29,27 +29,101 @@ def generate_dates_list(start_date_str, end_date_str):
     return dates_list
 
 
-if __name__ == "__main__":
+def plot_elements_comparison(bufor, date, discharge_nr, normalized, save_fig, plot):
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twiny()
+
+    colors = ["blue", "red"]
+
+    for idx, instance in enumerate(bufor):
+        element = instance.element
+        df = instance.df
+
+        plot_single_element(
+            ax1, ax2, df, element, date, discharge_nr, normalized, colors[idx]
+        )
+
+    configure_axes(ax1, ax2)
+    save_or_show_plot(bufor[0], date, discharge_nr, normalized, save_fig, plot)
+    plt.close()
+
+
+def plot_single_element(ax1, ax2, df, element, date, discharge_nr, normalized, color):
+    ax1.set_title(f"Comparison of Lyman-alpha intensities\n {date}.{discharge_nr:03}")
+    intensity = df[f"QSO_{element}_{date}.{discharge_nr}"]
+
+    if normalized:
+        intensity /= intensity.max()
+
+    ax1.plot(
+        pd.to_datetime(df["time"], format="%H:%M:%S.%f", errors="coerce"),
+        intensity,
+        alpha=0,
+    )
+
+    ax2.plot(
+        np.asarray(df["discharge_time"], float),
+        intensity,
+        label="discharge_time",
+        linewidth=0.4,
+        color=color,
+    )
+
+
+def configure_axes(ax1, ax2):
+    ax1.xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter("%H:%M:%S"))
+    ax1.tick_params(axis="x", rotation=45)
+    ax1.ticklabel_format(style="sci", axis="y", scilimits=(0, 0))
+    ax1.set_ylabel("Intensity [a.u.]")
+    ax1.set_xlabel("Local time")
+    ax2.set_xlabel("Discharge time [s]")
+    ax1.grid(which="major")
+    plt.tight_layout(rect=None)
+
+
+def save_or_show_plot(instance, date, discharge_nr, normalized, save_fig, plot):
+    if not (save_fig or plot):
+        return None
+
+    image_type = "normalized" if normalized else "original"
+    parent_path = instance.file_path_manager.images().parent.parent.parent
+    path = parent_path / "_Comparison" / date / image_type
+    path.mkdir(parents=True, exist_ok=True)
+
+    filename = (
+        f"QSO_comparison_norm_{date}.{discharge_nr:03}.png"
+        if normalized
+        else f"QSO_comparison_{date}.{discharge_nr:03}.png"
+    )
+
+    if save_fig:
+        plt.savefig(path / filename, dpi=200)
+        print("Comparyy")
+
+    if plot:
+        plt.show()
+
+
+def main():
+    time_interval = [0, 500]
+
     dates_list = generate_dates_list("20230101", "20230331")
     elements_list = ["C", "O"]
     discharges_list = [i for i in range(1, 100)]
 
     # dates_list = ["20230125"]
     # elements_list = ["C"]  # , "O"]
-    # discharges_list = [14]
+    # discharges_list = [13, 14]
 
-    time_interval = [0, 500]
+    for date in dates_list:
+        for discharge in discharges_list:
+            bufor = []
 
-    def collect_elements_data():
-        ...
-
-    bufor = []
-    for element in elements_list:
-        for date in dates_list:
-            for discharge in discharges_list:
+            for element in elements_list:
                 try:
                     f = DischargeFilesSelector(element, date, discharge)
                     discharge_files = f.discharge_files
+
                     for file_name in discharge_files:
                         bufor.append(
                             Intensity(
@@ -59,91 +133,24 @@ if __name__ == "__main__":
                                 file_name,
                                 time_interval,
                                 save_df=True,
-                                save_fig=True,
-                                plot=True,
+                                save_fig=False,
+                                plot=False,
                             )
                         )
                 except FileNotFoundError:
                     print(
                         f"{element}_{date}_{discharge} - No matching file found! Continue..."
                     )
-                    continue
 
-    def plot_all_elements(normalized=False, save_fig=False, plot=True):
-        fig, ax1 = plt.subplots()
-        ax2 = ax1.twiny()
-
-        colors = ["blue", "red"]
-
-        for idx, instance in enumerate(bufor):
-            element = instance.element
-            df = instance.df
-            date = instance.date
-            discharge_nr = instance.discharge_nr
-
-            ax1.set_title(
-                f"Comparison of Lyman-alpha intensities\n {date}.{discharge_nr:03}"
-            )
-            intensity = df[f"QSO_{element}_{date}.{discharge_nr}"]
-            if not normalized:
-                ax1.plot(
-                    pd.to_datetime(df["time"], format="%H:%M:%S.%f", errors="coerce"),
-                    intensity,
-                    alpha=0,
+            if len(bufor) >= 2:
+                parameters = (bufor, date, discharge)
+                plot_elements_comparison(
+                    *parameters, normalized=True, save_fig=True, plot=True
                 )
-                ax2.plot(
-                    np.asarray(df["discharge_time"], float),
-                    intensity,
-                    label="discharge_time",
-                    linewidth=0.4,
-                    color=colors[idx],
-                )
-            else:
-                ax1.plot(
-                    pd.to_datetime(df["time"], format="%H:%M:%S.%f", errors="coerce"),
-                    intensity / intensity.max(),
-                    alpha=0,
-                )
-                ax2.plot(
-                    np.asarray(df["discharge_time"], float),
-                    intensity / intensity.max(),
-                    label="discharge_time",
-                    linewidth=0.4,
-                    color=colors[idx],
+                plot_elements_comparison(
+                    *parameters, normalized=True, save_fig=True, plot=True
                 )
 
-            ax1.xaxis.set_major_formatter(
-                plt.matplotlib.dates.DateFormatter("%H:%M:%S")
-            )
-            ax1.tick_params(axis="x", rotation=45)
-            ax1.ticklabel_format(style="sci", axis="y", scilimits=(0, 0))
-            ax1.set_ylabel("Intensity [a.u.]")
-            ax1.set_xlabel("Local time")
-            ax2.set_xlabel("Discharge time [s]")
-            ax1.grid(which="major")
-            plt.tight_layout(rect=None)
 
-        def save_plot():
-            image_type = "normalized" if normalized else "original"
-
-            parent_path = instance.file_path_manager.images().parent.parent.parent
-            path = parent_path / "_Comparison" / date / image_type
-            path.mkdir(parents=True, exist_ok=True)
-
-            filename = (
-                f"QSO_comparison_norm_{date}.{discharge_nr:03}.png"
-                if normalized
-                else f"QSO_comparison_{date}.{discharge_nr:03}.png"
-            )
-
-            plt.savefig(path / filename, dpi=200)
-
-        if save_fig:
-            save_plot()
-        if plot:
-            plt.show()
-        plt.close()
-
-    if len(bufor) >= 2:
-        plot_all_elements(normalized=True, save_fig=True, plot=True)
-        plot_all_elements(normalized=False, save_fig=True, plot=True)
+if __name__ == "__main__":
+    main()
