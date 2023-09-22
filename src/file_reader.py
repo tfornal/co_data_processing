386095fce4b5daa95a30e6dc7fe079826029ba3f
tfornal <tomasz.fornal6@gmail.com>
2023-09-22 23:04:34 +0200

    refactoring

--- src/file_reader.py
+++ src/file_reader.py
@@ -10,7 +10,7 @@ class FilePathManager:
 
     main_path = pathlib.Path(__file__).parent.parent.resolve() / "data"
 
-    def __init__(self, element=None, date=None):
+    def __init__(self, element: str, date: str):
         self._element = element
         self._date = date
 
@@ -45,40 +45,7 @@ class FilePathManager:
         return self._get_path("time_evolutions", self.element, self.date, "img")
 
 
-class FileInformationCollector:
-    """Retrieves list of all files in a directory and their sizes."""
-
-    def __init__(self, path):
-        self.path = path
-        self.date = self.get_date_from_fnames()
-        self.file_list = self.get_file_list()
-        self.file_sizes = self.get_file_sizes()
-
-    def get_date_from_fnames(self):
-        return self.path.stem
-
-    def get_file_list(self):
-        """
-        Returns a list of file names in the directory.
-        """
-        directory = self.path.glob("**/*")
-        file_list = [x.stem for x in directory if x.is_file()]
-
-        return file_list
-
-    def get_file_sizes(self):
-        """
-        Returns a sizes of all files in a given directory.
-        """
-        directory = self.path.glob("**/*")
-        file_sizes = [
-            pathlib.Path(x).stat().st_size / 1024 for x in directory if x.is_file()
-        ]
-
-        return file_sizes
-
-
-class FileListExtractor:
+class FileListExtractor(FilePathManager):
     """Grabs all file names containing experimental and background data
     related to the given experimental discharge number."""
 
@@ -86,12 +53,6 @@ class FileListExtractor:
         self._element = element
         self._date = date
         self._discharge_nr = discharge_nr
-
-        self.fp = FilePathManager(self.element, self.date)
-        self.discharge_nr_file_path = self.fp.get_directory_for_exp_numbers()
-        self.exp_data_file_path = self.fp.get_directory_for_exp_data()
-        self.all_file_list = self.grab_all_file_list()
-        self.selected_file_names = self.select_file_names()
 
     @property
     def element(self) -> str:
@@ -105,15 +66,17 @@ class FileListExtractor:
     def discharge_nr(self) -> str:
         return self._discharge_nr
 
-    def grab_all_file_list(self):
-        return list(self.exp_data_file_path.glob("**/*"))
+    @classmethod
+    def grab_all_file_list(cls, path):
+        return list(path.glob("**/*"))
 
     def select_file_names(self):
         """Returns list of files that mathes the given date and discharge number."""
         df = pd.read_csv(
-            self.discharge_nr_file_path / f"{self.element}-{self.date}.csv", sep="\t"
+            self.get_directory_for_exp_numbers() / f"{self.element}-{self.date}.csv",
+            sep="\t",
         )
-        if self.discharge_nr != 0:
+        if self.discharge_nr > 0:
             df["discharge_nr"] = df["discharge_nr"].replace("-", "0").astype(int)
             selected_file_names = df.loc[df["discharge_nr"] == self.discharge_nr][
                 "file_name"
@@ -125,42 +88,63 @@ class BackgroundFilesSelector(FileListExtractor):
     """Grabs all file names containing background data
     related to the given experimental discharge number."""
 
-    def __init__(self, element, date, discharge_nr):
-        super().__init__(element, date, discharge_nr)
-        self.bgr_files = self.grab_bgr_files()
+    def _get_all_file_list(self):
+        return list(self.get_directory_for_exp_data().glob("**/*"))
 
-    def grab_all_file_list(self):
-        return list(self.exp_data_file_path.glob("**/*"))
-
-    def grab_bgr_files(self):
+    def get_bgr_file_list(self):
         bgr_files = [
             x
-            for x in self.all_file_list
-            if "BGR" in x.stem and x.stem in self.selected_file_names
+            for x in self._get_all_file_list()
+            if "BGR" in x.stem and x.stem in self.select_file_names()
         ]
         return bgr_files
 
 
-class DischargeFilesSelector(FileListExtractor):
+class ExperimentalFilesSelector(FileListExtractor):
     """Grabs all file names containing experimental data
     related to the given experimental discharge number."""
 
-    def __init__(self, element, date, discharge_nr):
-        super().__init__(element, date, discharge_nr)
-        self.discharge_files = self._grab_discharge_files()
-
-    def _grab_discharge_files(self):
+    def grab_discharge_files(self):
+        path = self.get_directory_for_exp_data()
+        file_list = self.grab_all_file_list(path)
         discharge_files = [
-            x
-            for x in self.all_file_list
-            if x.stat().st_size > 8000
-            and x.stem in self.selected_file_names
-            and "BGR" not in x.stem
+            file
+            for file in file_list
+            if file.stat().st_size > 8000
+            and file.stem in self.select_file_names()
+            and "BGR" not in file.stem
         ]
         return discharge_files
 
 
-class DischargeDataExtractor:
+class FileInformationCollector:
+    """Retrieves list of all files in a directory and their sizes."""
+
+    def grab_directory_content(cls, path):
+        return list(path.glob("**/*"))
+
+    def get_files_names(self, path) -> list:
+        """
+        Returns a list of file names in the directory.
+        """
+        files_list = self.grab_directory_content(path)
+        files_names = [x.stem for x in files_list if x.is_file()]
+
+        return files_names
+
+    def get_files_sizes(self, path):
+        """
+        Returns a sizes of all files in a given directory in kilobytes.
+        """
+        directory = self.grab_directory_content(path)
+        files_sizes = [
+            pathlib.Path(x).stat().st_size / 1024 for x in directory if x.is_file()
+        ]
+
+        return files_sizes
+
+
+class ExperimentalDataExtractor:
     def __init__(self, element, date, file_name):
         self.element = element
         self.date = date
