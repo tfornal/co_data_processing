e9eaad00c50cbcf9a42272a1395259b6cb2e7c6a
tfornal <tomasz.fornal6@gmail.com>
2023-10-04 07:24:02 +0200

    rewritten version to be checked - work in progress

--- src/triggers.py
+++ src/triggers.py
@@ -7,90 +7,86 @@ import pathlib
 from file_reader import FilePathManager
 
 
-class Triggers:
-    """Retrieves and processes information about experimental programs from the web API. The API returns information about programs in a JSON format."""
+# 1 - check if file exists
+# 2 - if not - establish connection and download from api
+# 3 - get triggers into dataframe; 
 
-    ARCHIVE_PROGINFO = "http://archive-webapi.ipp-hgw.mpg.de/programs.json?from="
 
-    def __init__(self, date: str, savefile=True):
-        """Initialize the instance of the class.
 
-        Parameters
-        ----------
-        date : str
-            A string in the format "YYMMDD" representing the date for which triggers data will be processed.
-        savefile : bool, optional
-            Whether the processed data should be saved as a csv file, by default False.
-        """
-        self.date = str(date)
-        self.trigger_path = FilePathManager().get_directory_for_program_triggers()
-        self.triggers_df = self.read_from_file()
-        breakpoint()
-        if self.triggers_df is None:
-            self.year, self.month, self.day = self.convert_date(self.date)
-            self.beginning_of_the_day, self.end_of_the_day = self._convert_to_utc()
-            self.url = self._get_url()
-            self.triggers = self._get_triggers_utc()
-            self.triggers_df = self._create_df()
-            if savefile:
-                self.save_file()
+class TriggersFromFile:
+    @classmethod
+    def _get_program_triggers_directory(cls):
+        fpm = FilePathManager()
+        trigger_directory_path = fpm.get_directory_for_program_triggers()
+        return trigger_directory_path
 
-    def read_from_file(self):
+    def _create_trigger_file_path(self, date: str) -> pathlib.Path:
+        trigger_directory_path = self._get_program_triggers_directory()
+        trigger_path = trigger_directory_path.joinpath(f"{date}_triggers.csv")
+        return trigger_path
+
+    def read_from_file(self, date):
+        path = self._create_trigger_file_path(date)
         try:
-            triggers_df = pd.read_csv(
-                self.trigger_path / f"{self.date}_triggers.csv", sep="\t"
-            )
+            triggers_df = pd.read_csv(path, sep="\t")
             return triggers_df
 
         except FileNotFoundError:
-            print(f"{self.date} local Trigger file does not exist. Downloading...")
+            print(f"{date} local Trigger file does not exist. Downloading...")
             return None
 
-    def convert_date(self, date: str) -> tuple:
-        year, month, day = (
-            int(date[:4]),
-            int(date[4:6]),
-            int(date[6:]),
-        )
-        return year, month, day
 
-    # TODO  -> alternatywna wersja ponizej; 
+
+class TriggersFromHTTP:
+
+    ARCHIVE_PROGINFO = "http://archive-webapi.ipp-hgw.mpg.de/programs.json?from="
+
+    # def __init__(self):
+        
+        # if not self.triggers_df:
+        # self.beginning_of_the_day, self.end_of_the_day = self._convert_to_utc()
+        # self.url = self._get_url()
+        # self.triggers = self._get_triggers_utc()
+        # self.triggers_df = self._create_df()
+        # if savefile:
+        #     self.save_file()
+
+    @classmethod
+    def _convert_date(cls, date: str) -> tuple:
+        try:
+            date_obj = datetime.strptime(date, "%Y%m%d")
+            year, month, day = date_obj.year, date_obj.month, date_obj.day
+            return year, month, day
+        except ValueError:
+            raise ValueError("Niepoprawny format daty. Oczekiwany format to YYYYMMDD.")
     
-    # def convert_date(date_str: str) -> tuple:
-    #     try:
-    #         date_obj = datetime.strptime(date_str, "%Y%m%d")
-    #         year, month, day = date_obj.year, date_obj.month, date_obj.day
-    #         return year, month, day
-    #     except ValueError:
-    #         raise ValueError("Niepoprawny format daty. Oczekiwany format to YYYYMMDD.")
+    @classmethod
+    def _compute_day_start_ns(self, converted_date):
+        start_of_the_day = datetime(converted_date*, 0, 0, 0, 0)
 
-    def _convert_to_utc(self):
-        """
-        Converts the `date` passed as parameter to UTC timestamps for the start and end of the day.
-
-        Returns
-        -------
-        tuple
-            A tuple of two integers representing the UTC timestamps for the start and end of the day of the `date` passed as parameter.
-        """
-
-        beginning_of_the_day = datetime(self.year, self.month, self.day, 0, 0, 0, 0)
-        end_of_the_day = datetime(self.year, self.month, self.day, 23, 59, 59, 0)
-
-        beginning_of_the_day = (
-            int(round(calendar.timegm(beginning_of_the_day.timetuple())))
+        start_time_in_ns = (
+            int(round(calendar.timegm(start_of_the_day.timetuple())))
             * 1_000_000_000
-            + beginning_of_the_day.microsecond * 1_000
-        )
-
-        end_of_the_day = (
-            int(round(calendar.timegm(end_of_the_day.timetuple()))) * 1_000_000_000
+            + start_of_the_day.microsecond * 1_000
+        )  
+        return start_time_in_ns
+    
+    @classmethod 
+    def _compute_day_end_ns(self, converted_date):
+        end_of_the_day = datetime(converted_date*, 23, 59, 59, 0)
+        end_time_in_ns = (
+            int(round(calendar.timegm(end_of_the_day.timetuple())))
+            * 1_000_000_000
             + end_of_the_day.microsecond * 1_000
-        )
+        )  
+        return end_time_in_ns
 
-        return beginning_of_the_day, end_of_the_day
+    def _grab_datetime_in_ns(self, converted_date):
+        day_start_ns = compute_day_start_ns(converted_date)
+        day_end_ns = compute_day_end_ns(converted_date)
+        return day_start_ns, day_end_ns
 
-    def _get_url(self):
+    def _get_url(self, timestamps_in_ns: tuple) -> str:
         """Returns the URL for accessing triggers data for the `date` passed as parameter."""
         url = (
             self.ARCHIVE_PROGINFO
@@ -99,6 +95,7 @@ class Triggers:
             + str(self.end_of_the_day)
         )
         return url
+    
 
     def _get_triggers_utc(self):
         """
@@ -139,7 +136,8 @@ class Triggers:
                 triggers["T6"].append(end_of_program)
         return triggers
 
-    def _create_df(self):
+
+    def get_triggers_df(self):
         """Creates a pandas DataFrame from the processed triggers data."""
         triggers_df = pd.DataFrame()
         triggers_df["discharge_nr"] = [
@@ -161,6 +159,12 @@ class Triggers:
         print("Triggers successfully saved!")
 
 
+class Triggers:
+    print("Trrr")
+
 if __name__ == "__main__":
-    date = "20230215"
-    df = Triggers(date, savefile=True).triggers_df
+    date = "20230323"
+    # triggers_df = TriggersFromFile().read_from_file(date)
+    triggers_df = TriggersFromHTTP()
+    print(triggers_df)
+    # Triggers()
